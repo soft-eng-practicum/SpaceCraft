c60500a6a16629219c548201cc714e0d4f0c63f4
Matthew Thompson <MatthewBiggieThompson@gmail.com>
2016-03-18 18:57:00 -0400

    added a Config.java file. set all statics variables to here.

---
+++ src/com/chromalife/SpaceInvaders/Config.java
@@ -0,0 +1,16 @@
+package com.chromalife.SpaceInvaders;
+
+public class Config
+{
+	public class java
+	{
+
+	}
+
+	//Height Width of App
+	public static final int WIDTH = 400,
+							HEIGHT = 800;
+	
+	
+	
+}
