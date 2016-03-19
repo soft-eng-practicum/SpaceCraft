1eb9db6c1db69b13d2ea04e7940aec3d833988a0
aabafogi <aabafogi@ggc.edu>
2016-03-19 14:52:10 -0400

    added controllers

---
+++ src/com/chromalife/SpaceInvadersControls/BulletControl.java
@@ -0,0 +1,32 @@
+package com.chromalife.SpaceInvadersControls;
+
+import com.almasb.ents.AbstractControl;
+import com.almasb.ents.Entity;
+import com.almasb.ents.component.ObjectComponent;
+import com.almasb.fxgl.entity.component.BoundingBoxComponent;
+import com.almasb.fxgl.entity.component.PositionComponent;
+import com.chromalife.SpaceInvaders.Config;
+import com.chromalife.SpaceInvaders.EntityCreator;
+
+public class BulletControl<OwnerComponent> extends AbstractControl {
+
+	private PositionComponent position;
+    private OwnerComponent owner;
+
+    private double speed;
+
+    public BulletControl(double speed) {
+        this.speed = speed;
+    }
+
+    @Override
+    public void onAdded(Entity entity) {
+     //   owner = entity.getControlUnsafe(OwnerComponent.class);
+        position = entity.getComponentUnsafe(PositionComponent.class);
+    }
+
+    @Override
+    public void onUpdate(Entity entity, double tpf) {
+        //position.translateY(owner.isType(EntityCreator.EntityType.PLAYER) ? -tpf * speed : tpf * speed);
+    }
+}
---
+++ src/com/chromalife/SpaceInvadersControls/LaserHitControl.java
@@ -0,0 +1,18 @@
+package com.chromalife.SpaceInvadersControls;
+
+import com.almasb.ents.AbstractControl;
+import com.almasb.ents.Entity;
+import com.almasb.ents.AbstractControl;
+import com.almasb.ents.Entity;
+import com.almasb.fxgl.entity.EntityView;
+import com.almasb.fxgl.entity.component.MainViewComponent;
+
+public class LaserHitControl extends AbstractControl {
+
+    private EntityView view;
+    
+    @Override
+    public void onAdded(Entity entity) {
+    	view = entity.
+    }
+}
