9167f2401f9e23e13881e14e1839fabb11376668
xaaviair <xlazo@ggc.edu>
2016-03-21 20:11:57 -0400

    Added the Junit test for the GameEvent file

---
+++ Testing/SpaceInvaders/Event/GameEventTest.java
@@ -0,0 +1,14 @@
+package SpaceInvaders.Event;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameEventTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
