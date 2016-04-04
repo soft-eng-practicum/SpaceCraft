84e19fa5dba17914dd57c7d8c2fd66a22e488383
xaaviair <xlazo@ggc.edu>
2016-04-04 16:45:59 -0400

    Created the BulletEnemyHandler to control collisions

---
+++ src/SpaceInvaders/Collision/BulletEnemyHandler.java
@@ -0,0 +1,70 @@
+package SpaceInvaders.Collision;
+
+import com.almasb.ents.Entity;
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.ServiceType;
+import com.almasb.fxgl.entity.Entities;
+import com.almasb.fxgl.entity.component.MainViewComponent;
+import com.almasb.fxgl.entity.component.PositionComponent;
+import com.almasb.fxgl.physics.CollisionHandler;
+
+import SpaceInvaders.EntityCreator;
+import SpaceInvaders.Components.HealthComponent;
+import SpaceInvaders.Components.OwnerComponent;
+import SpaceInvaders.Event.GameEvent;
+import javafx.geometry.Point2D;
+import javafx.scene.effect.BlendMode;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
+
+public class BulletEnemyHandler extends CollisionHandler
+{
+
+	public BulletEnemyHandler() {
+		super(EntityCreator.EntityType.BULLET, EntityCreator.EntityType.ENEMY);
+		
+	}
+	
+	protected void onCollisionBegin(Entity bullet, Entity enemy)
+	{
+			Object owner = bullet.getComponentUnsafe(OwnerComponent.class).getValue();
+			
+			if(owner == EntityCreator.EntityType.ENEMY)
+			{
+				return;
+			}
+			
+			Point2D hitPosition = bullet.getComponentUnsafe(PositionComponent.class	).getValue();
+			bullet.removeFromWorld();
+			
+			HealthComponent health = enemy.getComponentUnsafe(HealthComponent.class);
+			health.setValue(health.getValue() - 1);
+			
+			if(health.getValue() <= 0)
+			{
+				Entity explosion = EntityCreator.newExplosion(Entities.getBBox(enemy).getCenterWorld());
+				enemy.getWorld().addEntity(explosion);
+				
+				enemy.removeFromWorld();
+				
+				
+				GameApplication.getService(ServiceType.AUDIO_PLAYER).playSound("explosion.wav");
+				GameApplication.getService(ServiceType.EVENT_BUS).fireEvent(new GameEvent(GameEvent.ENEMY_KILLED));
+			} else {
+				Entity laserHit = EntityCreator.newLaserHit(hitPosition);
+				
+				enemy.getWorld().addEntity(laserHit);
+				
+				enemy.getComponentUnsafe(MainViewComponent.class).getView().setBlendMode(BlendMode.RED);
+				
+				GameApplication.getService(ServiceType.MASTER_TIMER)
+					.runOnceAfter(() -> {
+						if(enemy.isActive())
+							enemy.getComponentUnsafe(MainViewComponent.class).getView().setBlendMode(null);
+					}, Duration.seconds(0.33));
+			}
+	}
+	
+
+}
