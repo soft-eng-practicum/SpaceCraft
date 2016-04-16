5b633e473232ba30adc0e918b6be0e4a03c2c46d
FatThompson <MatthewBiggieThompson@gmail.com>
2016-04-16 12:25:47 -0400

    commented some things out to make the collision more functional. There is an error when fetching health data values, Will need to trouble shoot this as well.

--- src/SpaceCraft/Collision/BulletWallHandler.java
+++ src/SpaceCraft/Collision/BulletWallHandler.java
@@ -1,11 +1,13 @@
 package SpaceCraft.Collision;
 
 import com.almasb.ents.Entity;
+import com.almasb.fxgl.entity.component.PositionComponent;
 import com.almasb.fxgl.physics.CollisionHandler;
 
 import SpaceCraft.EntityCreator;
 import SpaceCraft.Components.HealthComponent;
 import SpaceCraft.Components.OwnerComponent;
+import javafx.geometry.Point2D;
 
 public class BulletWallHandler extends CollisionHandler
 {
@@ -18,14 +20,17 @@ public class BulletWallHandler extends CollisionHandler
 	{
 		Object owner = bullet.getComponentUnsafe(OwnerComponent.class).getValue();
 
-		if (owner == EntityCreator.EntityType.ENEMY)
+		if (owner == EntityCreator.EntityType.WALL)
 		{
-			bullet.removeFromWorld();
+		 return;//
+		}//
+		Point2D hitPosition = bullet.getComponentUnsafe(PositionComponent.class	).getValue();
+		bullet.removeFromWorld();
 
-			HealthComponent health = wall.getComponentUnsafe(HealthComponent.class);
-			health.setValue(health.getValue() - 1);
-			if(health.getValue() == 0)
+//			HealthComponent health = wall.getComponentUnsafe(HealthComponent.class);
+//			health.setValue(health.getValue() - 1);
+//			if(health.getValue() == 0)
 				wall.removeFromWorld();
-		}
+		//}
 	}
 }
